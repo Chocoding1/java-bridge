89d435d1c00f2b22600f8715553c653f0b2adc45
Chocoding1 <galmeagi2@naver.com>
2025-12-26 10:45:07 +0900

    Refactor(InputView): 다리 길이 입력 문구 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -7,10 +7,13 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class InputView {
 
+    private static final String INPUT_BRIDGE_SIZE_NOTICE = "다리의 길이를 입력해주세요.";
+
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
+        System.out.println(INPUT_BRIDGE_SIZE_NOTICE);
         String input = Console.readLine();
         try {
             return Integer.parseInt(input);
