d21863de65e18b6bceca1c46cd076514c886d0e2
Chocoding1 <galmeagi2@naver.com>
2025-12-27 16:24:37 +0900

    Refactor(InputView): 이동할 칸 입력 안내 문구 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -8,6 +8,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String INPUT_BRIDGE_SIZE_NOTICE = "다리의 길이를 입력해주세요.";
+    private static final String INPUT_MOVING_NOTICE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -26,6 +27,7 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
+        System.out.println(INPUT_MOVING_NOTICE);
         return Console.readLine();
     }
 
